18620007be2fbaed353de63c55e0e732b9618eb5
van_yog <ikharchenko2008@gmail.com>
2021-08-04 13:15:39 +0300

    add data for generate block

---
+++ src/components/Generate/Generate.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Generate = () => (
+  <div>
+    <div>
+      <h2>Why generate one?</h2>
+      <p>
+        Aside from helping the Nearkats in their mission of building out the
+        open web, and participating in a super fun game utilizing bleeding edge
+        tech, Nearkat NFTs will get you more than cute little kitty in a digital
+        city.
+      </p>
+      <p>Your Nearkat gets you:</p>
+      <p>
+        Special Access to Nearkat owners group Discounts on stuff Another perk
+        Another perk and who knows what else in the future...
+      </p>
+    </div>
+    <img src="./images/generate.png" alt="generate" />
+  </div>
+);
+
+export default Generate;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Hero from '../../components/Hero';
 import Learn from '../../components/Learn';
 import Rarity from '../../components/Rarity';
+import Generate from '../../components/Generate';
 
 const Home = () => (
   <>
     <Hero />
     <Learn />
     <Rarity />
+    <Generate />
   </>
 );
 
