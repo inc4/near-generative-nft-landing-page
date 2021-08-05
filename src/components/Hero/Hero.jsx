554e4f52647a4ca17e605e5e2785b2ace79bf8f9
van_yog <ikharchenko2008@gmail.com>
2021-08-05 17:49:00 +0300

    add container for hero block

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,16 +5,18 @@ import bigCat from '../../assets/images/hero-big-cat.svg';
 
 const Hero = () => (
   <div className="hero">
-    <h1 className="hero__title">
-      <span className="hero__title_red">Cute</span> is the new ‘
-      <span className="hero__title_red">cool</span>’
-    </h1>
-    <p className="hero__text">
-      Now you can own some of the cutest critters in the Metaverse.
-    </p>
-    <p className="hero__text hero__text_red">Sale Launches August 16th</p>
-    <CalendarBtn className="hero__calendar-btn" />
-    <ReactSVG className="hero__img" src={bigCat} />
+    <div className="hero__container">
+      <h1 className="hero__title">
+        <span className="hero__title_red">Cute</span> is the new ‘
+        <span className="hero__title_red">cool</span>’
+      </h1>
+      <p className="hero__text">
+        Now you can own some of the cutest critters in the Metaverse.
+      </p>
+      <p className="hero__text hero__text_red">Sale Launches August 16th</p>
+      <CalendarBtn className="hero__calendar-btn" />
+      <ReactSVG className="hero__img" src={bigCat} />
+    </div>
   </div>
 );
 
