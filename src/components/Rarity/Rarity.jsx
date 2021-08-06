8d65d00b121a0d173ade61c654a24aa41a7ba6a3
van_yog <ikharchenko2008@gmail.com>
2021-08-06 15:45:35 +0300

    add styles for rarity block

--- src/components/Rarity/Rarity.jsx
+++ src/components/Rarity/Rarity.jsx
@@ -1,23 +1,31 @@
 import React from 'react';
+import { ReactSVG } from 'react-svg';
 import NearkatsList from '../NearkatsList';
 import RateList from '../RateList';
 import rarityData from './rarityData';
+import gradient from '../../assets/images/rarity-background-gradient.svg';
 
 const Rarity = () => (
   <section className="rarity">
-    <div>
-      <h1>10,000 Nearkats... How rare is yours?</h1>
-      <p className="rarity__text">
-        All Nearkats are cute, however, some are more rare than others. The
-        rarity of your Nearkat is determined by its attributes.
-        <br />
-        Each has been given a score to represent how rare it is. There is also a
-        fancy color system to help you distinquish.
-      </p>
-    </div>
-    <div>
+    <div className="rarity__header">
+      <h1 className="rarity__title">10,000 Nearkats... How rare is yours?</h1>
       <RateList rates={rarityData.rates} />
-      <NearkatsList kats={rarityData.kats} />
+      <ReactSVG className="rarity__header-background" src={gradient} />
+    </div>
+    <div className="rarity__content-wrapper">
+      <div className="rarity__content">
+        <div className="rarity__information">
+          <p className="rarity__text">
+            All Nearkats are cute, however, some are more rare than others. The
+            rarity of your Nearkat is determined by its attributes.
+          </p>
+          <p className="rarity__text">
+            Each has been given a score to represent how rare it is. There is
+            also a fancy color system to help you distinquish.
+          </p>
+        </div>
+        <NearkatsList kats={rarityData.kats} />
+      </div>
     </div>
   </section>
 );
--- src/components/RateList/RateList.jsx
+++ src/components/RateList/RateList.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import RateListItem from './RateListItem';
 
 const RateList = ({ rates }) => (
-  <div>
+  <div className="rate-list">
     {rates.length &&
       rates.map((rate) => <RateListItem key={rate.id} rate={rate} />)}
   </div>
