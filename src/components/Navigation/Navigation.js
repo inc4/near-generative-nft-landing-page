4533893b6897f722c52193d3aec5e78c540263a1
Ivan <ikharchenko2008@gmail.com>
2021-09-30 10:56:00 +0300

    make navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,77 +1,60 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-const Navigation = ({ className, signedIn }) => {
-  const handleClick = (e) => {
-    e.preventDefault();
-
-    const id = e.target.dataset.href;
-    const elem = document.getElementById(id);
-
-    elem.scrollIntoView({
-      behavior: 'smooth',
-    });
-  };
-
-  return (
-    <nav className={`navigation ${className || ''}`}>
-      <ul className="navigation__list">
+const Navigation = ({ className, signedIn }) => (
+  <nav className={`navigation ${className || ''}`}>
+    <ul className="navigation__list">
+      <li className="navigation__list-item">
+        <Link
+          to="/#learn"
+          className="navigation__link"
+          data-href="learn"
+          href="#learn"
+        >
+          Learn
+        </Link>
+      </li>
+      <li className="navigation__list-item">
+        <Link
+          to="/#rarity"
+          className="navigation__link"
+          data-href="rarity"
+          href="#rarity"
+        >
+          Rarity
+        </Link>
+      </li>
+      <li className="navigation__list-item">
+        <Link
+          to="/#faq"
+          className="navigation__link"
+          data-href="faq"
+          href="#faq"
+        >
+          FAQ
+        </Link>
+      </li>
+      <li className="navigation__list-item">
+        <Link
+          to="/#roadmap"
+          className="navigation__link"
+          data-href="roadmap"
+          href="#roadmap"
+        >
+          Roadmap
+        </Link>
+      </li>
+      {signedIn && (
         <li className="navigation__list-item">
-          <a
-            className="navigation__link"
-            onClick={handleClick}
-            data-href="learn"
-            href="#learn"
-          >
-            Learn
-          </a>
+          <Link to="/my-nfts" className="navigation__link">
+            My NFTs
+          </Link>
         </li>
-        <li className="navigation__list-item">
-          <a
-            className="navigation__link"
-            onClick={handleClick}
-            data-href="rarity"
-            href="#rarity"
-          >
-            Rarity
-          </a>
-        </li>
-        <li className="navigation__list-item">
-          <a
-            className="navigation__link"
-            onClick={handleClick}
-            data-href="faq"
-            href="#faq"
-          >
-            FAQ
-          </a>
-        </li>
-        <li className="navigation__list-item">
-          <a
-            className="navigation__link"
-            onClick={handleClick}
-            data-href="roadmap"
-            href="#roadmap"
-          >
-            Roadmap
-          </a>
-        </li>
-        {signedIn && (
-          <li className="navigation__list-item">
-            <a
-              className="navigation__link"
-              onClick={handleClick}
-              data-href="roadmap"
-              href="#roadmap"
-            >
-              My NFTs
-            </a>
-          </li>
-        )}
-      </ul>
-    </nav>
-  );
-};
+      )}
+    </ul>
+  </nav>
+);
 
 Navigation.propTypes = {
   className: PropTypes.string,
