6a02d8fed20392eb0d4eeeec97ade79ec4ddef05
Ivan <ikharchenko2008@gmail.com>
2021-09-30 10:59:40 +0300

    add NftList for kats

--- src/pages/MyNFTS/MyNFTS.js
+++ src/pages/MyNFTS/MyNFTS.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import NftList from '../../components/NftList/NftList';
+import data from './dataNfts';
 
-const MyNFTS = () => <div>MyNFTS page</div>;
+const MyNFTS = () => (
+  <div className="my-nfts">
+    <div className="my-nfts__data">
+      <NftList list={data} />
+    </div>
+  </div>
+);
 
 export default MyNFTS;
