aeec950a7afdc026e6aeec7bb86fde1cb6465538
Ivan <ikharchenko2008@gmail.com>
2021-10-04 18:24:40 +0300

    add scroll to top component for scrolling page

--- src/components/RenderRoutes/RenderRoutes.js
+++ src/components/RenderRoutes/RenderRoutes.js
@@ -2,14 +2,18 @@ import React from 'react';
 import { Route, Switch } from 'react-router';
 import PropTypes from 'prop-types';
 import RouteWithSubRoutes from './RouteWithSubRoutes';
+import ScrollToTop from '../ScrollToTop';
 
 const RenderRoutes = ({ routes }) => (
-  <Switch>
-    {routes.map((route) => (
-      <RouteWithSubRoutes key={route.key} {...route} />
-    ))}
-    <Route component={() => <h1>Page not found!</h1>} />
-  </Switch>
+  <>
+    <Switch>
+      {routes.map((route) => (
+        <RouteWithSubRoutes key={route.key} {...route} />
+      ))}
+      <Route component={() => <h1>Page not found!</h1>} />
+    </Switch>
+    <ScrollToTop />
+  </>
 );
 
 RenderRoutes.propTypes = {
