afae516dd2b6f3fcb2efd408570da7affe18514e
Ivan <ikharchenko2008@gmail.com>
2021-10-06 12:27:30 +0300

    add new information about nearkat

--- src/components/NftItemInfo/NftItemInfo.js
+++ src/components/NftItemInfo/NftItemInfo.js
@@ -2,21 +2,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { ReactSVG } from 'react-svg';
 
-const NftItemInfo = ({ className, item, onMouseEnter, onMouseLeave }) => (
-  <div
-    className={`nft-item-info  ${className || ''}`}
-    onMouseEnter={onMouseEnter}
-    onMouseLeave={onMouseLeave}
-  >
-    <div>Info about KAT</div>
-    <ReactSVG src={item.icon} />
+const NftItemInfo = ({ className, item }) => (
+  <div className={`nft-item-info  ${className || ''}`}>
+    <div className="nft-item-info__icon-wrapper">
+      <ReactSVG className="nft-item-info__icon" src={item.icon} />
+      <p className="nft-item-info__number">{item.number}</p>
+      <ReactSVG className="nft-item-info__rarity" src={item.rarity} />
+    </div>
+    <div className="nft-item-info__stats">
+      <p>Name:&nbsp;Uncommon</p>
+      <p>Head: </p>
+      <p>Background: </p>
+      <p>Clothing</p>
+      <p>Generation</p>
+      <p>Sequence</p>
+      <p>Eyewear</p>
+      <p>Fur / Skin </p>
+      <p>Attribute Count</p>
+      <p>Twin</p>
+      <p>Mouth</p>
+    </div>
   </div>
 );
 
 NftItemInfo.propTypes = {
   className: PropTypes.string,
   item: PropTypes.object,
-  onMouseLeave: PropTypes.func,
-  onMouseEnter: PropTypes.func,
 };
 export default NftItemInfo;
