03b1bae0908102da7315aa66b9e8dbf4a4d82fc3
Ivan <ikharchenko2008@gmail.com>
2021-10-06 12:26:08 +0300

    del unused code

--- src/components/NftItem/NftItem.js
+++ src/components/NftItem/NftItem.js
@@ -1,44 +1,19 @@
 /* eslint-disable no-console */
-import React, { useState } from 'react';
+import React from 'react';
 import { ReactSVG } from 'react-svg';
 import PropTypes from 'prop-types';
 import NftItemInfo from '../NftItemInfo';
 
-const NftItem = ({ item }) => {
-  const [on, setOn] = useState(false);
-  console.log('on: ', on);
-
-  const onMouseEnter = () => {
-    console.log('Enter');
-    setOn(true);
-  };
-
-  const onMouseLeave = () => {
-    setOn(false);
-    console.log('Leave');
-  };
-  return (
-    <div className="nft-item">
-      <ReactSVG
-        className="nft-item__icon"
-        src={item.icon}
-        onMouseEnter={onMouseEnter}
-        onMouseLeave={onMouseLeave}
-      />
-      <div className="nft-item__footer">
-        <p className="nft-item__hash">{item.number}</p>
-        <ReactSVG src={item.rarity} />
-      </div>
-      {on && (
-        <NftItemInfo
-          className="nft-item__info"
-          item={item}
-          onMouseLeave={onMouseLeave}
-        />
-      )}
+const NftItem = ({ item }) => (
+  <div className="nft-item">
+    <ReactSVG className="nft-item__icon" src={item.icon} />
+    <div className="nft-item__footer">
+      <p className="nft-item__hash">{item.number}</p>
+      <ReactSVG src={item.rarity} />
     </div>
-  );
-};
+    <NftItemInfo className="nft-item__info" item={item} />
+  </div>
+);
 
 NftItem.propTypes = {
   item: PropTypes.object,
