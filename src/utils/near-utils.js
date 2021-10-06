e410ae3d014d650c6eaafb80575ccf344d3c7b01
Ivan <ikharchenko2008@gmail.com>
2021-10-06 17:30:13 +0300

    add getContract

--- src/utils/near-utils.js
+++ src/utils/near-utils.js
@@ -2,14 +2,9 @@
 import * as nearAPI from 'near-api-js';
 import getConfig from '../config';
 
-export const {
-  networkId, nodeUrl, walletUrl,
-} = getConfig();
+export const { networkId, nodeUrl, walletUrl, contractName } = getConfig();
 
-const {
-  Account,
-  Contract,
-} = nearAPI;
+const { Account, Contract } = nearAPI;
 
 export const formatAccountId = (accountId, len = 16) => {
   if (accountId.length > len) {
@@ -20,8 +15,18 @@ export const formatAccountId = (accountId, len = 16) => {
 
 export const getWallet = async () => {
   const near = await nearAPI.connect({
-    networkId, nodeUrl, walletUrl, deps: { keyStore: new nearAPI.keyStores.BrowserLocalStorageKeyStore() },
+    networkId,
+    nodeUrl,
+    walletUrl,
+    deps: { keyStore: new nearAPI.keyStores.BrowserLocalStorageKeyStore() },
   });
   const wallet = new nearAPI.WalletAccount(near);
   return { near, wallet };
+};
+
+export const getContract = (account, methods = contractMethods) => {
+  return new nearAPI.Contract(account, contractName, {
+    ...methods,
+    sender: account.accountId,
+  });
 };
