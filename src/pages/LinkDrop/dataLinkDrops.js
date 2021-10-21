161c8c157509dac4ca92d97c69f09c55e2c46390
Ivan <ikharchenko2008@gmail.com>
2021-10-21 16:52:31 +0300

    add fake data

---
+++ src/pages/LinkDrop/dataLinkDrops.js
@@ -0,0 +1,29 @@
+const linkDrops = [
+  {
+    key: 1,
+    link: 'https://wallet.testnet.near.org/linkdrop/joinwallet.testnet/4tFTKj5jEMG6ykc7gkKP3ma1s8Gv5JLERkPkvhMuzLg1r5aXFFo4Qs3qBBPaNVKDqj11AWikhoy5u138FVkFDME1?redirectUrl=http://localhost:3001/link-drop',
+    text: 'for my friend',
+  },
+  {
+    key: 2,
+    link: 'https://wallet.testnet.near.org/linkdrop/joinwallet.testnet/5H6sKUYyKeLMKfpA2P3Jc5rNkVn99h3vZdfxg36vUjNdHaCk1hwxzsA3UqEqz27aV8vkh1HCZboEmrwWTmgZBuGB?redirectUrl=http://localhost:3001/link-drop',
+    text: 'happy birthday',
+  },
+  {
+    key: 3,
+    link: 'https://wallet.testnet.near.org/linkdrop/joinwallet.testnet/2KkGaufJ4JkRbnCnWhHHxz3wcZrxiHWHi11AxduJ9Mq2x3FPSZqxV2TaSCuKGeHSZA5nYHu2LTqXUqzm9ZVDhXeS?redirectUrl=http://localhost:3001/link-drop',
+    text: 'for Ted',
+  },
+  {
+    key: 4,
+    link: 'https://wallet.testnet.near.org/linkdrop/joinwallet.testnet/3vfiSYMAXaTsr5qDUpxEynHMXSMwtYbBxAW5xyYY8bhqRDCcLDdpTHeFMtV8ePu3aLdx2FteiqtWcbC5hyVHLp9r?redirectUrl=http://localhost:3001/link-drop',
+    text: '',
+  },
+  {
+    key: 5,
+    link: 'https://wallet.testnet.near.org/linkdrop/joinwallet.testnet/4szVTzKEHufrGsR6ciNdvNttJucDrXx5nJGFxD9De3VaZ624qPyScVz2ofw5wmGpoGeQsZd4GBwmy5VDt24YAQeJ?redirectUrl=http://localhost:3001/link-drop',
+    text: 'from Jack to Sara',
+  },
+];
+
+export default linkDrops;
